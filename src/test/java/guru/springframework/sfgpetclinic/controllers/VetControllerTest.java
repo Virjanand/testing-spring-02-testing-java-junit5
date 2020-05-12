7926dc9361f12304672f7a2dbd2916a0304f9928
Virjanand <virjanand@virjanand.com>
2020-05-12 06:00:34 +0200

    Add tags for tests so you can target which kind of tests to run/group of tests

--- src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -7,14 +7,15 @@ import guru.springframework.sfgpetclinic.services.SpecialtyService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import guru.springframework.sfgpetclinic.services.map.SpecialityMapService;
 import guru.springframework.sfgpetclinic.services.map.VetMapService;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
+@Tag("controllers")
 class VetControllerTest {
 
     @Test
