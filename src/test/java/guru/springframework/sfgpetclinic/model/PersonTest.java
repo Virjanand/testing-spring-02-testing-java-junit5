7e9247b6f5394167084f3652f6ff2cd9913b2928
Virjanand <virjanand@virjanand.com>
2020-05-20 05:51:06 +0200

    added repeated test

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -17,7 +19,7 @@ class PersonTest implements ModelTests {
         assertAll("Test props set",
                 () -> assertEquals("Joe", person.getFirstName()),
                 () -> assertEquals("Buck", person.getLastName())
-                );
+        );
     }
 
     @Test
@@ -29,6 +31,12 @@ class PersonTest implements ModelTests {
         assertAll("Test props set",
                 () -> assertEquals("Joe", person.getFirstName(), "First name failed"),
                 () -> assertEquals("Buck", person.getLastName(), "Last name failed")
-                );
+        );
+    }
+
+    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions}")
+    @DisplayName("My Repeated Test")
+    void myRepeatedTest() {
+        // implementation not needed to demonstrate
     }
 }
