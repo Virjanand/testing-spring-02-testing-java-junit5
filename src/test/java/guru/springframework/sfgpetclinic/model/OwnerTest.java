5bc7000afca47360a9c00b4bf57a3632c28849f4
Virjanand <virjanand@virjanand.com>
2020-05-23 07:49:38 +0200

    added a parameterized test with value source and displayname

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,7 +1,10 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -20,13 +23,20 @@ class OwnerTest implements ModelTests {
                 () -> assertAll("Person Properties",
                         () -> assertEquals("Joe", owner.getFirstName()),
                         () -> assertEquals("Buck", owner.getLastName())
-                        ),
+                ),
                 () -> assertAll("Owner Properties",
                         () -> assertEquals("Key West", owner.getCity()),
                         () -> assertEquals("1231231234", owner.getTelephone())
-                        )
-                );
+                )
+        );
 
         assertThat(owner.getCity(), is("Key West"));
+    }
+
+    @DisplayName("Value Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @ValueSource(strings = {"Sprint", "Framework", "Guru"})
+    void testValueSource(String val) {
+        System.out.println(val);
     }
 }
